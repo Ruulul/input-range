198cb7c6ffc28cbf4fdb53cb42c2535594e49601
Ruulul <vamjunior01@gmail.com>
2023-01-03 13:59:12 -0300

    new demo

--- demo/demo.js
+++ demo/demo.js
@@ -1,4 +1,17 @@
 const InputRange = require('..')
 
-const x = InputRange()
-document.body.appendChild(x)
+const age_input = InputRange()
+const year_input = InputRange()
+const page = document.createElement('div')
+page.innerHTML = `
+    <h1> Demo input </h1>
+    <label> Enter your age 
+        <age-input></age-input>
+    </label>
+    <label> Enter your birth year
+        <year-input></year-input>
+    </label>
+`
+page.querySelectorAll('age-input').forEach(e=>e.replaceWith(age_input))
+page.querySelectorAll('year-input').forEach(e=>e.replaceWith(year_input))
+document.body.appendChild(page)
