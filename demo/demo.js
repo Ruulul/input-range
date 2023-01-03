70a4cbdb5ae23f7c24041999450febd109d7be47
Ruulul <vamjunior01@gmail.com>
2023-01-03 14:25:02 -0300

    handling potential multiple components

--- demo/demo.js
+++ demo/demo.js
@@ -1,9 +1,5 @@
 const InputRange = require('..')
 
-const max_age = 150
-const age_input = InputRange({min: 0, max: max_age})
-const actual_year = 2022
-const year_input = InputRange({min: actual_year - max_age, max: actual_year})
 const page = document.createElement('div')
 page.innerHTML = `
     <h1> Demo input </h1>
@@ -14,6 +10,8 @@ page.innerHTML = `
         <year-input></year-input>
     </label>
 `
-page.querySelectorAll('age-input').forEach(e=>e.replaceWith(age_input))
-page.querySelectorAll('year-input').forEach(e=>e.replaceWith(year_input))
+const max_age = 150
+const actual_year = 2022
+page.querySelectorAll('age-input').forEach(e=>e.replaceWith(InputRange({min: 0, max: max_age})))
+page.querySelectorAll('year-input').forEach(e=>e.replaceWith(InputRange({min: actual_year - max_age, max: actual_year})))
 document.body.appendChild(page)
