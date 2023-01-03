140dea7a36f0004c830ae80e98dda5d75e22c940
Ruulul <vamjunior01@gmail.com>
2023-01-03 13:52:12 -0300

    type number, handling range

--- src/index.js
+++ src/index.js
@@ -7,11 +7,31 @@ const e = document.createElement.bind(document)
 function InputRange () {
     const el = e('div')
     const shadow = el.attachShadow({mode: 'closed'})
+    
     const input = e('input')
+    input.type = 'number'
+    input.min = 0
+    input.max = 150
+    input.onkeyup = handlers.onkeyup
+    
     shadow.appendChild(input)
     shadow.adoptedStyleSheets = [sheet]
 
     return el
+}
+
+const handlers = {
+    onkeyup(e) {
+        actions.ensureRange(e)
+    }
+}
+const actions = {
+    ensureRange(e) {
+        const el = e.target
+        const val = new Number(el.value)
+        if (val > el.max) el.value = el.max
+        else if (val < el.min) el.value = el.min
+    }
 }
 
 function getTheme () {
