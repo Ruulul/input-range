2ee7e912f842ffbb1ffb0df2f3c751c0c93bd8ca
Ruulul <vamjunior01@gmail.com>
2023-01-03 14:06:39 -0300

    options; waits to type

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ const sheet = new CSSStyleSheet
 sheet.replaceSync(getTheme())
 
 const e = document.createElement.bind(document)
-function InputRange () {
+function InputRange ({min = 0, max = 100} = {min: 0, max: 100}) {
     const el = e('div')
     const shadow = el.attachShadow({mode: 'closed'})
     
     const input = e('input')
     input.type = 'number'
-    input.min = 0
-    input.max = 150
+    input.min = min
+    input.max = max
     input.onkeyup = handlers.onkeyup
     
     shadow.appendChild(input)
@@ -28,6 +28,9 @@ const handlers = {
 const actions = {
     ensureRange(e) {
         const el = e.target
+        const len_min = el.min.toString().length
+        const len_typed = el.value.toString().length
+        if (len_typed < len_min) return
         const val = new Number(el.value)
         if (val > el.max) el.value = el.max
         else if (val < el.min) el.value = el.min
