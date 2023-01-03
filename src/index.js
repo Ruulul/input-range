a94f68833f126a23409af1c955a714e02ce2470e
Ruulul <vamjunior01@gmail.com>
2023-01-03 20:43:41 -0300

    scoped handlers to include notify

--- src/index.js
+++ src/index.js
@@ -4,10 +4,35 @@ const sheet = new CSSStyleSheet
 sheet.replaceSync(getTheme())
 
 const e = document.createElement.bind(document)
-function InputRange ({min = 0, max = 100} = {min: 0, max: 100}) {
+function InputRange({ min = 0, max = 100 } = { min: 0, max: 100 }, notify) {
+    const handlers = {
+        onkeyup(e) {
+            if (utils.isWiderThan(e.target.min, e.target.value)) return
+            actions.ensureRange(e)
+        },
+        onleave(e) {
+            if (utils.isWiderThan(e.target.min, e.target.value)) return e.target.value = ''
+            actions.ensureRange(e)
+        }
+    }
+    const utils = {
+        isWiderThan(any1, any2) {
+            return any1.toString().length > any2.toString().length
+        }
+    }
+    const actions = {
+        ensureRange(e) {
+            const el = e.target
+            const val = new Number(el.value)
+            if (val > el.max) el.value = el.max
+            else if (val < el.min) el.value = el.min
+            if (notify) notify({ type: 'update', body: el.value })
+        }
+    }
+
     const el = e('div')
-    const shadow = el.attachShadow({mode: 'closed'})
-    
+    const shadow = el.attachShadow({ mode: 'closed' })
+
     const input = e('input')
     input.type = 'number'
     input.min = min
@@ -15,38 +40,14 @@ function InputRange ({min = 0, max = 100} = {min: 0, max: 100}) {
     input.onkeyup = handlers.onkeyup
     input.onpointerleave = handlers.onleave
     input.onblur = handlers.onleave
-    
+
     shadow.appendChild(input)
     shadow.adoptedStyleSheets = [sheet]
 
     return el
 }
 
-const handlers = {
-    onkeyup(e) {
-        if (utils.isWiderThan(e.target.min, e.target.value)) return
-        actions.ensureRange(e)
-    },
-    onleave(e) {
-        if (utils.isWiderThan(e.target.min, e.target.value)) return e.target.value = ''
-        actions.ensureRange(e)
-    }
-}
-const utils = {
-    isWiderThan(any1, any2) {
-        return any1.toString().length > any2.toString().length
-    }
-}
-const actions = {
-    ensureRange(e) {
-        const el = e.target
-        const val = new Number(el.value)
-        if (val > el.max) el.value = el.max
-        else if (val < el.min) el.value = el.min
-    }
-}
-
-function getTheme () {
+function getTheme() {
     return `
         input {
             padding: 0.5em 1em;
